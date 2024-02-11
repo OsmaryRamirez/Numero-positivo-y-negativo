6b595eb3b238abb32b3e75e92648ad37b278394b
Osmary <Osmary@DESKTOP-8DOM8BM>
2024-02-11 10:33:00 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wdkwfjkwj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("numero positivo");
+            Console.WriteLine();
+
+            //leer un numero entero positivo y cuando sea negativo, el bucle debe parar.
+            //mostrar un mensaje que diga que ha introducido un numero negativo.
+
+            int numero;
+
+            Console.Write("introduzca el numero entero positivo  ");
+            numero = int.Parse(Console.ReadLine());
+
+
+            do
+            {
+                Console.Write("introduzca un numero entero positivo");
+                numero = int.Parse(Console.ReadLine());
+
+            } while (numero > 0);
+
+            Console.WriteLine("ha introducido un numero negativo");
+
+        }
+    }
+}
